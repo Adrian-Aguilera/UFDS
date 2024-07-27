d8942058a6d1f1ebf0feb0540798b3aebd31c931
Adrian-Aguilera <cruz74270@gmail.com>
2024-07-27 17:49:51 -0600

    feat(GUIA#1): buttons add

---
+++ adriantds221b/src/components/OptionsComponents.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="navbar navbar-expand-sm bg-light justify-content-center">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="#conoce">Conoce tu Biblioteca</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#servicios">Servicios</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#recursos">Recursos</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#bibliografia">Nueva Bibliografía</a>
+        </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'OptionsButtons',
+  props: {
+    msg: String
+  }
+}
+</script>
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ adriantds221b/src/pages/main.vue
@@ -0,0 +1,12 @@
+<template>
+    <OptionsButtons/>
+</template>
+<script>
+import OptionsButtons from '@components/OptionsComponents.vue'
+export default {
+    name: 'mainPage',
+    components: {
+        OptionsButtons
+    }
+}
+</script>
