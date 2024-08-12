38354ca2ffd2ac76cb75ef83b96297f7bcc2a5c8
Adrian-Aguilera <cruz74270@gmail.com>
2024-08-11 18:22:22 -0600

    P-II: cards

--- P-II/aguilera-tds221-b/src/App.vue
+++ P-II/aguilera-tds221-b/src/App.vue
@@ -1,9 +1,48 @@
 <template>
-  <div class="mx-5 flex">
+  <div class="mx-5 flex place-content-evenly">
     <CardsComponent
       urlImagen="https://www.itca.edu.sv/wp-content/uploads/2016/11/Ctecla.jpg"
       titulo="Sede Santa Tecla"
       MasInfo="https://www.itca.edu.sv/carreras-santa-tecla/"
+      Informacion="Carreras impartidas en la sede de santa tecla, la libertad"
+    />
+    <CardsComponent
+    urlImagen="https://www.itca.edu.sv/wp-content/uploads/2016/11/Cmiguel.jpg"
+    titulo="Sede Regional San Miguel"
+    MasInfo="https://www.itca.edu.sv/carreras-san-miguel/"
+    Informacion="Carreras impartidas en la sede de San Miguel"
+    />
+    <CardsComponent
+      urlImagen="https://www.itca.edu.sv/wp-content/uploads/2016/11/Cana.jpg"
+      titulo="Sede Regional Santa Ana"
+      MasInfo="https://www.itca.edu.sv/carreras-santa-ana/"
+      Informacion="Carreras impartidas en la sede de Santa Ana"
+    />
+  </div>
+  <div class="mx-5 flex place-content-evenly">
+    <CardsComponent
+      urlImagen="https://www.itca.edu.sv/wp-content/uploads/2016/11/Cunion.jpg"
+      titulo="Sede La Union"
+      MasInfo="https://www.itca.edu.sv/carreras-la-union/"
+      Informacion="Carreras impartidas en la sede de La Union"
+    />
+    <CardsComponent
+    urlImagen="https://www.itca.edu.sv/wp-content/uploads/2016/11/Czaca.jpg"
+    titulo="Sede Regional Zacatecoluca"
+    MasInfo="https://www.itca.edu.sv/carreras-zacatecoluca/"
+    Informacion="Carreras impartidas en la sede de Zacatecoluca"
+    />
+    <CardsComponent
+    urlImagen="https://www.itca.edu.sv/wp-content/uploads/2019/09/carreras_virtualesmin.jpg"
+    titulo="Carreras Semi Presenciales"
+    MasInfo="https://www.itca.edu.sv/formulario-carreras-virtuales/"
+    Informacion="Modalidad Semi Presencia!"
+    />
+    <CardsComponent
+    urlImagen="https://www.itca.edu.sv/wp-content/uploads/2019/10/carreras-duales-min.jpg"
+    titulo="Carreras Duales!"
+    MasInfo="https://www.itca.edu.sv/educacion-dual/"
+    Informacion="Modalidad Dual!"
     />
   </div>
 </template>
