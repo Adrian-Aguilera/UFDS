8b29fdc01380bf049c48959c7f70a2ae4de4a516
Adrian-Aguilera <cruz74270@gmail.com>
2024-09-14 18:16:02 -0600

    inicio de la logica

--- pe-vii/src/components/CuestionarioComponent.vue
+++ pe-vii/src/components/CuestionarioComponent.vue
@@ -1,11 +1,47 @@
 <template>
-    <div>
-        
+    <div class="container">
+        <div class="form-check">
+            <div v-for="datos in preguntasArray" :key="datos.id">
+                <p> {{ datos.pregunta }} </p>
+                <div style="display: flex;">
+                    <input class="form-check-input" v-model="respuesta" type="checkbox" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                      {{ datos.opcion1 }}
+                    </label>
+                </div>
+                <div style="display: flex;">
+                    <input class="form-check-input" v-model="respuesta" type="checkbox" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                      {{ datos.opcion2 }}
+                    </label>
+                </div>
+                <div style="display: flex;">
+                    <input class="form-check-input" v-model="respuesta" type="checkbox" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                      {{ datos.opcion3 }}
+                    </label>
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 <script>
 export default {
     name: 'CuestionarioComponent',
+    props:{
+        preguntasArray:Array
+    },
+    data() {
+        return {
+            respuesta: ''
+        }
+    },
+    watch:{
+        respuesta(nuevoValor, valorAnterior) {
+            console.log('El mensaje cambió de:', valorAnterior, 'a:', nuevoValor);
+        }
+    }
 }
 
 </script>
--- pe-vii/src/views/CuestionarioView.vue
+++ pe-vii/src/views/CuestionarioView.vue
@@ -1,10 +1,24 @@
 <template>
     <div>
         <h2>Cuestionario</h2>
+        <CuestionarioComponent :preguntasArray="valoresPregunta"/>
     </div>
 </template>
 <script>
+import CuestionarioComponent from '@/components/CuestionarioComponent.vue'
     export default {
         name:'CuestionarioView',
+        components:{
+            CuestionarioComponent,
+        },
+        data() {
+            return {
+                valoresPregunta:[
+                    {id:1, pregunta:'que hace vue?', opcion1:'es un framework', opcion2:'es un framwork js', opcion3: 'xd'},
+                    {id:1, pregunta:'que hace vue?1', opcion1:'es un framework1', opcion2:'es un framwork js', opcion3: 'xd'},
+                    {id:1, pregunta:'que hace vue?2', opcion1:'es un framework2', opcion2:'es un framwork js', opcion3: 'xd'},
+                ]
+            }
+        },
     }
 </script>
