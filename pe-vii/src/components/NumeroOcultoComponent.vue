39cdc7b86695195916a6780edf44e4d202dec7ca
Adrian-Aguilera <cruz74270@gmail.com>
2024-09-16 17:17:08 -0600

    ejercicio  4 done

--- pe-vii/src/components/NumeroOcultoComponent.vue
+++ pe-vii/src/components/NumeroOcultoComponent.vue
@@ -3,7 +3,13 @@
         <h6>Ingresa un numero hasta adivinarlo!</h6>
         <div class="input-group mb-3">
             <span class="input-group-text"> {{ texto }} </span>
-            <input type="text" class="form-control" aria-label="Dollar amount (with dot and two decimal places)">
+            <input type="number" v-model="numero" class="form-control" aria-label="Dollar amount (with dot and two decimal places)">
+        </div>
+        <div v-if="numeroEncontrado === true" class="alert alert-success" role="alert">
+            Demaciado alto
+        </div>
+        <div v-else class="alert alert-danger" role="alert">
+            Demaciado bajo
         </div>
     </div>
 </template>
@@ -13,6 +19,36 @@ export default {
     name: 'NumeroOcultoComponent',
     props:{
         texto:String,
+    },
+    data() {
+        return {
+            numero: null,
+            numeroEncontrado: false
+        }
+    },
+    methods:{
+        encontrarNumero(numero){
+            const numeroOculto = 10;
+            const diferencia = Math.abs( numero - numeroOculto)
+
+            console.log('>>', diferencia)
+            if (diferencia < numeroOculto ){
+                console.log('has encontrado el numero correcto')
+                this.numeroEncontrado = true
+            }
+            else{
+                console.log('el numero esta lejos!')
+                this.numeroEncontrado =  false
+            }
+        }
+    },
+    watch:{
+        numero:{
+            deep:true,
+            handler(){
+                    this.encontrarNumero(this.numero);
+            }
+        }
     }
 }
 </script>
