17bee7be28b8a7a7742d102af4070f7f45127f5d
Adrian-Aguilera <cruz74270@gmail.com>
2024-09-30 17:30:43 -0600

    feat: contador vista inicial

---
+++ pe-viii/src/components/ContadorComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Contador</h1>
+        <p>Valor: {{ contador }}</p>
+        <button @click="incrementar">Incrementar</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            contador: 0
+        }
+    },
+    methods: {
+        incrementar() {
+            this.contador++
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ pe-viii/src/views/ContadorVista.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>vista del contador 1</h1>
+        <p>componente:</p>
+        <ContadorComponent />
+    </div>
+</template>
+<script>
+import ContadorComponent from '@/components/ContadorComponent.vue'
+export default {
+    name: 'ContadorVista',
+    components: {
+        ContadorComponent
+    }
+
+}
+</script>
