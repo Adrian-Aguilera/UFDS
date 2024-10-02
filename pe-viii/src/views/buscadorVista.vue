2e8b43724914f308ab657bf2fd6b8b5226d7e1bd
LIGT-BIT <cruz74270@gmail.com>
2024-10-01 22:14:34 -0600

    tabla agregada

--- pe-viii/src/views/buscadorVista.vue
+++ pe-viii/src/views/buscadorVista.vue
@@ -4,6 +4,38 @@
         <BuscadorProductoComponent />
     </div>
     <AddProductosComponent />
+    <div class="pa-md-4 mx-lg-auto">
+
+        <v-alert class="text-center rounded-0" type="info" max-width="5000"> 
+
+            <v-table class="mx-auto">
+                <thead>
+                  <tr>
+                    <th class="text-left">
+                      Nombre producto
+                    </th>
+                    <th class="text-left">
+                      Precio
+                    </th>
+                    <th class="text-left">
+                        Stock
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr
+                    v-for="item in this.$store.getters.obtenerProductosStocks"
+                    :key="item.nombre"
+                  >
+                    <td>{{ item.nombre }}</td>
+                    <td>{{ item.precio }}</td>
+                    <td>{{ item.stock }}</td>
+                  </tr>
+                </tbody>
+            </v-table>
+    
+        </v-alert>
+    </div>
 </template>
 <script>
 import BuscadorProductoComponent from '../components/BuscadorProductoComponent.vue'
